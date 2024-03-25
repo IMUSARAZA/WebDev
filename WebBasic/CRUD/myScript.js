d83c27e069d6683a3a134381b0c38b54b601fbc4
Musa Raza <imusaraza@gmail.com>
2024-03-25 23:18:52 +0500

    CRUD Operations Added

--- WebBasic/CRUD/myScript.js
+++ WebBasic/CRUD/myScript.js
@@ -1,27 +1,52 @@
-console.log("Hello World");
+// Load posts
+loadPosts();
 
-loadStories();
-
-function loadStories() {
+function loadPosts() {
   $.ajax({
     url: "https://jsonplaceholder.typicode.com/posts",
     method: "GET",
     error: function(response) {
       console.error("An error has occurred:", response); 
     },
-    success: storiesResponseArrived,
+    success: postsResponseArrived,
   }); 
 }
 
-function storiesResponseArrived(data) {
-  
-  data.forEach(function(stories) {
- 
-    console.log("iD:", stories.id);
-    console.log("title:", stories.title);
-    console.log("body:", stories.body);
+function postsResponseArrived(data) {
+  data.forEach(function(post) {
+    console.log("ID:", post.id);
+    console.log("Title:", post.title);
+    console.log("Body:", post.body);
   });
 }
 
+// Delete
+function deletePost(postId) {
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/posts/" + postId,
+    method: "DELETE",
+    error: function(response) {
+      console.error("An error has occurred while deleting post:", response); 
+    },
+    success: function() {
+      console.log("Post with ID", postId, "has been deleted successfully.");
+    }
+  });
+}
 
-
+// Replace
+function replacePost(postId, newPostData) {
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/posts/" + postId,
+    method: "PUT",
+    contentType: "application/json",
+    data: JSON.stringify(newPostData),
+    error: function(response) {
+      console.error("An error has occurred while replacing post:", response); 
+    },
+    success: function() {
+      console.log("Post with ID", postId, "has been replaced successfully.");
+    }
+  });
+}
+a
