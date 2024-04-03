2b0d9b16fc92178f4909a1eacf5a2f89838d6060
Musa Raza <imusaraza@gmail.com>
2024-04-03 13:37:24 +0500

    2nd commit

--- Mid/WebsiteClone(Lab Exam)/myScript.js
+++ Mid/WebsiteClone(Lab Exam)/myScript.js
@@ -2,3 +2,8 @@ document.querySelector(".leftImage-1").addEventListener("mouseover", function() 
     var imageName = this.alt; 
     document.querySelector(".select-1 option").textContent = imageName; 
 });
+
+document.querySelector(".left-image2").addEventListener("mouseover", function() {
+    var imageName = this.alt; 
+    document.querySelector(".select-1 option").textContent = imageName; 
+});
