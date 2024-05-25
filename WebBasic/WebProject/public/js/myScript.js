770712d2ee3dfe7c3ce513798f2680dc4dc2d3ee
Musa Raza <musarazach@gmail.com>
2024-05-26 03:45:53 +0500

    Some changes done

--- WebBasic/WebProject/public/js/myScript.js
+++ WebBasic/WebProject/public/js/myScript.js
@@ -36,28 +36,28 @@ navMenu.classList.remove('show-menu')
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER WACTHES ===============*/
-const swiperWatches = new Swiper('.home__swiper', {
-    loop: true,
-    spaceBetween: 32,
-    grabCursor: true,
-    effect: 'creative',
-    creativeEffect: {
-        prev: {
-            translate: [-100, 0, -500],
-            rotate: [0, 0, 15],
-            opacity: 0,
+    const swiperWatches = new Swiper('.home__swiper', {
+        loop: true,
+        spaceBetween: 32,
+        grabCursor: true,
+        effect: 'creative',
+        creativeEffect: {
+            prev: {
+                translate: [-100, 0, -500],
+                rotate: [0, 0, 15],
+                opacity: 0,
+            },
+            next: {
+                translate: [-100, 0, -500],
+                rotate: [0, 0, 15],
+                opacity: 0,
+            }
         },
-        next: {
-            translate: [-100, 0, -500],
-            rotate: [0, 0, 15],
-            opacity: 0,
-        }
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
 
 /*=============== GSAP ANIMATION ===============*/
